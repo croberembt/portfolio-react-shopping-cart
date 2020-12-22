872c220d9a03b8d3d69e7dddccc2e0d7669bccb1
Calli Oberembt <croberembt@gmail.com>
2020-12-21 19:48:32 -0600

    working on modal

--- src/components/ProductComponent.js
+++ src/components/ProductComponent.js
@@ -2,33 +2,34 @@ import React, { Component } from 'react';
 import { Button, Card, CardBody, CardHeader, CardImg, CardFooter, Modal, ModalBody, ModalHeader, ModalFooter } from 'reactstrap';
 import formatCurrency from '../util'; 
 import Fade from 'react-reveal/Fade';
-import Zoom from 'react-reveal/Zoom';
 
 export default class ProductComponent extends Component {
 
     constructor(props) {
         super(props);
         this.state= {
-            isModalOpen: false
+            product: null
         };
-        this.toggleModal = this.toggleModal.bind(this);
     }
 
-    toggleModal() {
-        this.setState({
-            isModalOpen: !this.state.isModalOpen
-        });
+    openModal = (product) => {
+        this.setState({product}); 
+    }
+
+    closeModal = () => {
+        this.setState({product: null});
     }
 
     render() {
+        const {product} = this.state;
         const productList = this.props.products.map(product => {
             return (
                 <Fade bottom>
                     <div key={product._id} className='col-md-6'>
                         <Card style={{marginBottom: '2rem'}}>
                             <CardBody>
-                                <CardHeader className='text-center product-title'><a href='#' onClick={this.toggleModal}>{product.title}</a></CardHeader>
-                                <CardImg onClick={this.toggleModal} src={product.image} alt={product.description}></CardImg>
+                                <CardHeader className='text-center product-title'><a href='#' onClick={() => this.openModal(product)}>{product.title}</a></CardHeader>
+                                <CardImg onClick={() => this.openModal(product)} src={product.image} alt={product.description}></CardImg>
                                 <CardFooter>
                                     <p>{product.description}</p>
                                     <p>{formatCurrency(product.price)}</p>
@@ -49,23 +50,23 @@ export default class ProductComponent extends Component {
                     {productList}
                 </div>
                 <div className='row'>
-                    {
-                        <div className='col-md-6'>
-                            <Modal className='text-center product-title' isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    {product && ( 
+                        <div className='col-md-6'> 
+                            <Modal className='text-center product-title' isOpen={true} onRequestClose={this.closeModal}>
                                 <ModalBody>
-                                    <ModalHeader toggle={this.toggleModal}>{this.props.product.title}</ModalHeader>
-                                    <CardImg onClick={this.toggleModal} src={this.props.product.image} alt={this.props.product.description}></CardImg>
+                                    <ModalHeader onClick={this.closeModal}>{product.title}</ModalHeader>
+                                    <CardImg onClick={this.toggleModal} src={product.image} alt={product.description}></CardImg>
                                     <ModalFooter>
-                                        <p>{this.props.product.description}</p>
-                                        <p>{formatCurrency(this.props.product.price)}</p>
+                                        <p>{product.description}</p>
+                                        <p>{formatCurrency(product.price)}</p>
                                     </ModalFooter>
-                                    <Button onClick={() => this.props.addToCart(product)}  color='warning' style={{margin: '1rem'}}>
+                                    <Button onClick={() => {this.props.addToCart(product); this.closeModal()}}  color='warning' style={{margin: '1rem'}}>
                                         Add To Cart
                                     </Button> 
                                 </ModalBody>
                             </Modal>
                         </div> 
-                    }
+                    )}
                 </div>
             </div>
         );
