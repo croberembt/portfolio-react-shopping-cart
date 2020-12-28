e11fba0ae6764b26faa99185d25dcf7b898dbade
Calli Oberembt <croberembt@gmail.com>
2020-12-28 17:54:08 -0600

    implemented fetch products action

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -1,12 +1,13 @@
 import {FETCH_PRODUCTS} from '../types'; 
 
-export const fetchProducts = () => async(dispatch) = {
+export const fetchProducts = () => async (dispatch) => {
     
     const res = await fetch('/api/products');
-   
+    const data = await res.json(); 
+
     dispatch({
         type: FETCH_PRODUCTS,
-        payload: res.data,
+        payload: data
     });
     
 }
--- src/components/ProductComponent.js
+++ src/components/ProductComponent.js
@@ -28,36 +28,37 @@ class ProductComponent extends Component {
 
     render() {
         const {product} = this.state;
-        const productList = this.props.products.map(product => {
-            return (
-                <Fade bottom>
-                    <div key={product._id} className='col-md-6'>
-                        <Card style={{marginBottom: '2rem'}}>
-                            <CardBody>
-                                <CardHeader className='text-center product-title'><a href='#' onClick={() => this.openModal(product)}>{product.title}</a></CardHeader>
-                                <CardImg onClick={() => this.openModal(product)} src={product.image} alt={product.description}></CardImg>
-                                <CardFooter>
-                                    <p>{product.description}</p>
-                                    <p>{formatCurrency(product.price)}</p>
-                                </CardFooter>
-                                <Button onClick={() => this.props.addToCart(product)} color='warning' style={{margin: '1rem'}}>
-                                    Add To Cart
-                                </Button> 
-                            </CardBody>
-                        </Card>
-                    </div>
-                </Fade>
-            );
-        });
-
         return (
             <div className='container'>
                 <div className='row'>
-                    {productList}
+                    <Fade bottom>
+                        {
+                            !this.props.products ? (<div style={{fontWeight: 'bold', color: 'white'}}>Loading...</div>) :
+                            <div className='row'>
+                                {this.props.products.map(product => 
+                                    <div className='col-md-6'>
+                                        <Card key={product._id} style={{marginBottom: '2rem'}}>
+                                            <CardBody>
+                                                <CardHeader className='text-center product-title'><a href='#' onClick={() => this.openModal(product)}>{product.title}</a></CardHeader>
+                                                <CardImg onClick={() => this.openModal(product)} src={product.image} alt={product.description}></CardImg>
+                                                <CardFooter>
+                                                    <p>{product.description}</p>
+                                                    <p>{formatCurrency(product.price)}</p>
+                                                </CardFooter>
+                                                <Button onClick={() => this.props.addToCart(product)} color='warning' style={{margin: '1rem'}}>
+                                                    Add To Cart
+                                                </Button> 
+                                            </CardBody>
+                                        </Card>
+                                    </div>
+                                )}
+                            </div>
+                        }
+                    </Fade>
                 </div>
                 <div className='row'>
                     {product && ( 
-                        <div className='col-md-6'> 
+                        <div> 
                             <Modal className='product-title' isOpen={true} onRequestClose={this.closeModal}>
                                 <ModalBody>
                                     <Button className='float-right' onClick={() => this.closeModal()}>x</Button>
@@ -80,4 +81,4 @@ class ProductComponent extends Component {
     }
 }
 
-export default connect((state) => ({products: state.products.items}), {fetchProducts}) (ProductComponent); 
+export default connect((state) => ({products: state.products.items}), {fetchProducts,}) (ProductComponent); 
