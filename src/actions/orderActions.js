c00e82f1ba22c89487f8c4452bb57b8b90de5b6b
Calli Oberembt <croberembt@gmail.com>
2021-01-06 18:58:55 -0600

    working on create order using redux and mongodb

---
+++ src/actions/orderActions.js
@@ -0,0 +1,28 @@
+import { CLEAR_CART, CREATE_ORDER } from "../types"
+
+export const createOrder = (order) => (dispatch) => {
+    fetch('/api/orders', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(order)
+    })
+    .then(res => res.json())
+    .then(data => {
+        dispatch(
+            { 
+                type: CREATE_ORDER, 
+                payload: data
+            }
+        );
+        localStorage.clear('cartItems'); 
+        dispatch(
+            {
+                type: CLEAR_CART
+            }
+        ); 
+    })
+}
+
+export const clearOrder = 
