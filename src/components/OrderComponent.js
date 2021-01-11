a37c2e5c05e19a0d00530f248634e1bb38a18c1e
Calli Oberembt <croberembt@gmail.com>
2021-01-11 12:38:01 -0600

    added order history table for admin and styled

--- src/components/OrderComponent.js
+++ src/components/OrderComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchOrders } from '../actions/orderActions'; 
 import formatCurrency from '../util'; 
+import { Table } from 'reactstrap';
 
 class OrderComponent extends Component {
 
@@ -11,31 +12,51 @@ class OrderComponent extends Component {
 
     render() {
         return (
-            
+
                 <div className='container'>
                     {
-                    !orders ? 
-                    <div>
-                        ...Loading
-                    </div>
-                    :
-                    <div>
-                        {this.props.orders.map(order => 
-                           <div>
-                                {order._id}
-                                {order.createdAt}
-                                {formatCurrency(order.total)}
-                                {order.name}
-                                {order.email}
-                                {order.address}
-                                {order.cartItems.map(item => 
-                                    <div>
-                                        {item.count} {'x'} {item.title}
-                                    </div>
-                                )}
+                        !this.props.orders ? 
+                        <div>
+                            ...Loading
+                        </div>
+                        :
+                        <div>
+                            <div>
+                                <h3 className='text-center' style={{marginBottom: '1rem', color: 'white'}}>Order History</h3>
                             </div>
-                        )}
-                    </div>
+                            <Table responsive bordered hover size='sm'>
+                                <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Date</th>
+                                        <th>Name</th>
+                                        <th>Email</th>
+                                        <th>Shipping Address</th>
+                                        <th>Items Ordered</th>
+                                        <th>Total</th>
+                                    </tr>
+                                </thead>
+                                {this.props.orders.map(order => 
+                                    <tbody>
+                                        <tr>
+                                            <th scope="row">{order._id}</th>
+                                            <td>{order.createdAt}</td>
+                                            <td>{order.name}</td>
+                                            <td>{order.email}</td>
+                                            <td>{order.address}</td>
+                                            <td>                                 
+                                                {order.cartItems.map(item => 
+                                                    <div>
+                                                        {item.count} {'x'} {item.title}
+                                                    </div>
+                                                )}
+                                            </td>
+                                            <td>{formatCurrency(order.total)}</td>
+                                        </tr>
+                                    </tbody>
+                                )}
+                            </Table>
+                        </div>
                     }
                 </div>
             
